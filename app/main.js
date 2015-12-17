dae3aed202cd0aba74f1272db885bd2f641f0747
gin280 <gin280@163.com>
2015-12-17 13:31:46 +0800

    '1'

---
+++ app/main.js
@@ -0,0 +1,7 @@
+'use strict';
+var component = require('./component/component.js');
+var style = require('./scss/main.scss');
+
+// import component from 'component.js';
+
+document.body.appendChild(component());
